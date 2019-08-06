3165750c88de86c4c3079b96a5eb5e160e4ddc56
Mackmilan <andre.mackmilan@gmail.com>
2019-08-05 23:19:25 -0300

    Pack upate

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
@@ -12,11 +13,11 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-        
-        .package(url: "/Users/andresilva/dev/swift/UIKit", .branch("master"))
+
+//        .package(url: "/Users/andresilva/dev/swift/UIKit", .branch("master"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Leaf", "UIKit", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Leaf", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
